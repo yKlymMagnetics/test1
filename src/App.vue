500529a3b2449a5c7469e331bcb0eded63f0bc87
unknown <yar@magnetics.io>
2021-06-29 13:35:00 +0300

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    Hello world
+
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav>
+    <a href="*">Home</a>
+    <br />
+    <a href="*">Users</a>
+  </nav>
+</template>
